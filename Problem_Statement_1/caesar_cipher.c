6e812a17ee4db6faede19cb5da7da4024d01e4c8
Adity-shivam <adishivam1507@gmail.com>
2025-08-22 00:49:07 +0530

    Added stuff

--- Problem_Statement_1/caesar_cipher.c
+++ Problem_Statement_1/caesar_cipher.c
@@ -11,15 +11,15 @@ void take_input(char string[MAXCHAR]) {
   string[i] = '\0';
 }
 
-void encode(char INPUT[MAXCHAR], char OUTPUT[MAXCHAR]) {
+void encode(char INPUT[MAXCHAR], char OUTPUT[MAXCHAR], int shift) {
   int i = 0;
   while (INPUT[i] != '\0') {
-    if ((INPUT[i] >= 'a' && INPUT[i] <= 'z' - SHIFT) ||
-        (INPUT[i] >= 'A' && INPUT[i] <= 'Z' - SHIFT)) {
-      OUTPUT[i] = INPUT[i] + SHIFT;
-    } else if ((INPUT[i] > 'z' - SHIFT && INPUT[i] <= 'z') ||
-               (INPUT[i] > 'Z' - SHIFT && INPUT[i] <= 'Z')) {
-      OUTPUT[i] = INPUT[i] - 26 + SHIFT;
+    if ((INPUT[i] >= 'a' && INPUT[i] <= 'z' - shift) ||
+        (INPUT[i] >= 'A' && INPUT[i] <= 'Z' - shift)) {
+      OUTPUT[i] = INPUT[i] + shift;
+    } else if ((INPUT[i] > 'z' - shift && INPUT[i] <= 'z') ||
+               (INPUT[i] > 'Z' - shift && INPUT[i] <= 'Z')) {
+      OUTPUT[i] = INPUT[i] - 26 + shift;
     } else {
       OUTPUT[i] = INPUT[i];
     }
@@ -27,15 +27,15 @@ void encode(char INPUT[MAXCHAR], char OUTPUT[MAXCHAR]) {
   }
 }
 
-void decode(char INPUT[MAXCHAR], char OUTPUT[MAXCHAR]) {
+void decode__(char INPUT[MAXCHAR], char OUTPUT[MAXCHAR], int shift) {
   int i = 0;
   while (INPUT[i] != '\0') {
-    if ((INPUT[i] >= 'a' + SHIFT && INPUT[i] <= 'z') ||
-        (INPUT[i] >= 'A' + SHIFT && INPUT[i] <= 'Z')) {
-      OUTPUT[i] = INPUT[i] - SHIFT;
-    } else if ((INPUT[i] >= 'a' && INPUT[i] < 'a' + SHIFT) ||
-               (INPUT[i] >= 'A' && INPUT[i] < 'A' + SHIFT)) {
-      OUTPUT[i] = INPUT[i] - SHIFT + 26;
+    if ((INPUT[i] >= 'a' + shift && INPUT[i] <= 'z') ||
+        (INPUT[i] >= 'A' + shift && INPUT[i] <= 'Z')) {
+      OUTPUT[i] = INPUT[i] - shift;
+    } else if ((INPUT[i] >= 'a' && INPUT[i] < 'a' + shift) ||
+               (INPUT[i] >= 'A' && INPUT[i] < 'A' + shift)) {
+      OUTPUT[i] = INPUT[i] - shift + 26;
     } else {
       OUTPUT[i] = INPUT[i];
     }
@@ -60,6 +60,81 @@ void decode(char INPUT[MAXCHAR], char OUTPUT[MAXCHAR]) {
  (echo d && cat encoded.c) | ./a.out | tail -n +3 > decoded.c
   */
 
+void get_freq(char INPUT[MAXCHAR], double frequency[26]) {
+
+  // make sure to initialize frequency[26] = {0}
+  int count = 0;
+  for (int i = 0; INPUT[i] != '\0' && i < MAXCHAR; i++) {
+    if (INPUT[i] >= 'a' && INPUT[i] <= 'z') {
+      frequency[INPUT[i] - 'a']++;
+      count++;
+    }
+    else if (INPUT[i] >= 'A' && INPUT[i] <= 'Z') {
+      frequency[INPUT[i] - 'A']++;
+      count++;
+    }
+  }
+
+  for (int j = 0; j < 26; j++)
+    frequency[j] = frequency[j] / count;
+}
+
+/* i want to make a function which will match this freq array with actual
+   english alphabet data i will use something called chi-squared distance to
+   measure deviation from actual frequency
+
+   Found the expex_freq value from
+   https://mathcenter.oxford.emory.edu/site/math125/englishLetterFreqs/
+   */
+
+double chi_square(double act_freq[26]) {
+
+  double expec_freq[26] = {0.08167, 0.01492, 0.02782, 0.04253, 0.12702, 0.02228,
+                           0.02015, 0.06094, 0.06966, 0.00153, 0.00772, 0.04025,
+                           0.02406, 0.06749, 0.07507, 0.01929, 0.00095, 0.05987,
+                           0.06327, 0.09056, 0.02758, 0.00978, 0.02360, 0.00150,
+                           0.01974, 0.00074};
+
+  double chi_square = 0;
+
+  for (int i = 0; i < 26; i++)
+    chi_square = chi_square + ((act_freq[i] - expec_freq[i]) *
+                               (act_freq[i] - expec_freq[i]) / expec_freq[i]);
+
+  return chi_square;
+}
+
+int decode_shift(char string[MAXCHAR]) {
+
+  double shifted_freq[26] = {0};
+  char temp[MAXCHAR];
+  double variance;
+  double minimum;
+  int shift = 0;
+  int calc_shift = 0;
+
+  take_input(string);
+
+  decode__(string, temp, shift);
+  get_freq(temp, shifted_freq);
+  variance = chi_square(shifted_freq);
+  minimum = variance;
+
+  for (int shift = 1; shift < 26; shift++) {
+
+    decode__(string, temp, shift);
+    get_freq(temp, shifted_freq);
+    variance = chi_square(shifted_freq);
+
+    if (variance < minimum) {
+      minimum = variance;
+      calc_shift = shift;
+    }
+  }
+
+  return calc_shift;
+}
+
 int main() {
   char INPUT[MAXCHAR] = {0};
   char OUTPUT[MAXCHAR] = {0};
@@ -67,16 +142,26 @@ int main() {
 
   printf("Enter \"e\" to encode and \"d\" to decode the cipher : ");
   scanf("%c", &oper);
-  printf("Enter Text [Press ctrl D twice to end input]: \n");
-  take_input(INPUT);
 
   switch (oper) {
   case 'e': {
-    encode(INPUT, OUTPUT);
+
+    printf("enter shift value : ");
+    int shift;
+    scanf("%d", &shift);
+    printf("Enter Text [Press ctrl D twice to end input]: \n");
+    take_input(INPUT);
+    encode(INPUT, OUTPUT, shift);
     break;
   }
   case 'd': {
-    decode(INPUT, OUTPUT);
+    printf("Enter Text [Press ctrl D twice to end input]: \n");
+    take_input(INPUT);
+
+    int shift;
+    shift = decode_shift(INPUT);
+    decode__(INPUT, OUTPUT, shift);
+
     break;
   }
   }
